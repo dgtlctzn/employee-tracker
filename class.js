8cfb4b9378b47e429adac14884a96ebc4c4e6f42
Joseph Perry <josephperry720@gmail.com>
2020-10-04 18:44:03 -0400

    add department method

--- class.js
+++ class.js
@@ -1,14 +1,28 @@
 const mysql = require("mysql");
 
 class Database {
+  constructor() {
+    this.connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "katsu",
+      database: "employee_db",
+    });
+    this.connection.connect(function (err) {
+      if (err) throw err;
+    });
+  }
 
-    constructor() {
-        this.connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "katsu",
-            database: "employee_db"
-          })
-    }
+  addDepartment(department) {
+      this.connection.query("INSERT INTO department SET ?", {
+          name: department,
+      }, (err) => {
+          if (err) throw err;
+          this.connection.end();
+      })
+  }
 }
+
+const db = new Database;
+db.addDepartment("HR");
