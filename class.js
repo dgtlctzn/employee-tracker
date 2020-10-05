77029e63e72955668b56f7ce35cbd3a4c7901525
Joseph Perry <josephperry720@gmail.com>
2020-10-05 15:19:42 -0400

    changed template literal to ?

--- class.js
+++ class.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 
 class Database {
-    
   constructor() {
     this.connection = mysql.createConnection({
       host: "localhost",
@@ -52,7 +51,7 @@ class Database {
 
   viewTable(table) {
     return new Promise((resolve, reject) => {
-      this.connection.query(`SELECT * FROM ${table}`, (err, res) => {
+      this.connection.query("SELECT * FROM ??", [table], (err, res) => {
         if (err) {
           reject();
         }
@@ -66,3 +65,13 @@ class Database {
     this.connection.end();
   }
 }
+
+const db = new Database();
+db.viewTable("role")
+  .then(() => {
+    db.endConnection();
+  })
+  .catch((err) => {
+    if (err) console.log(err);
+  });
+// db.endConnection();
